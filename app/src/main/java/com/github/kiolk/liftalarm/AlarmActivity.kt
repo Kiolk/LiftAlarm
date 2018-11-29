8488326f6bd5e8dd55574769351f488d3cc54932
eugene.slizh <Moov2Ej1>
2018-11-29 10:57:51 +0300

    Update gradle plugin version. Add command in jenkinsfile

--- app/src/main/java/com/github/kiolk/liftalarm/AlarmActivity.kt
+++ app/src/main/java/com/github/kiolk/liftalarm/AlarmActivity.kt
@@ -2,11 +2,17 @@ package com.github.kiolk.liftalarm
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.crashlytics.android.Crashlytics
+import io.fabric.sdk.android.Fabric
+
+
+
 
 class AlarmActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_alarm)
+        Fabric.with(this, Crashlytics())
     }
 }
